b33404fea16c535882f04cd11ff52ee206fee222
黄建国 <1490451648@qq.com>
2018-01-31 09:15:54 +0700

    Fix

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# flexBoxDemo
-flexBoxDemo
+# flexboxDemo
---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './App';
+import flexbox from './flexbox';
+
+AppRegistry.registerComponent('flexboxDemo', () => flexbox);
